87469def4acb8aada1d5779d5a637b1988f94493
Gaurav Sharma <gsharma81999@gmail.com>
2021-01-07 11:51:17 +0530

    minor modification in yolo detector

--- detectors/yolo_detector.py
+++ detectors/yolo_detector.py
@@ -12,21 +12,21 @@ class YoloDetector(BaseDetector):
     def __init__(self, initial_frame: np.ndarray, is_valid_cntrarea=None, sub_type=None) -> None:
         super().__init__(is_valid_cntrarea, sub_type)
 
-        self.config_path = "D:/darknet/build/darknet/x64/cfg/yolov4.cfg"
-        if not os.path.exists(self.config_path):
-            raise ValueError("Invalid config path `" + os.path.abspath(self.config_path)+"`")
+        config_path = "D:/darknet/build/darknet/x64/cfg/yolov4.cfg"
+        if not os.path.exists(config_path):
+            raise ValueError("Invalid config path `" + os.path.abspath(config_path)+"`")
 
-        self.weight_path = "D:/darknet/build/darknet/x64/yolov4.weights"
-        if not os.path.exists(self.weight_path):
-            raise ValueError("Invalid weight path `" + os.path.abspath(self.weight_path)+"`")
+        weight_path = "D:/darknet/build/darknet/x64/yolov4.weights"
+        if not os.path.exists(weight_path):
+            raise ValueError("Invalid weight path `" + os.path.abspath(weight_path)+"`")
 
-        self.meta_path = "D:/darknet/build/darknet/x64/cfg/coco.data"
-        if not os.path.exists(self.meta_path):
-            raise ValueError("Invalid data file path `" + os.path.abspath(self.meta_path)+"`")
+        meta_path = "D:/darknet/build/darknet/x64/cfg/coco.data"
+        if not os.path.exists(meta_path):
+            raise ValueError("Invalid data file path `" + os.path.abspath(meta_path)+"`")
 
-        self.net_main = darknet.load_net_custom(self.config_path.encode("ascii"), self.weight_path.encode("ascii"), 0, 1)  # batch size = 1
+        self.net_main = darknet.load_net_custom(config_path.encode("ascii"), weight_path.encode("ascii"), 0, 1)  # batch size = 1
 
-        self.meta_main = darknet.load_meta(self.meta_path.encode("ascii"))
+        self.meta_main = darknet.load_meta(meta_path.encode("ascii"))
 
         try:
             with open(self.meta_main) as metaFH:
